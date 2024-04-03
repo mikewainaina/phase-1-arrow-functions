a05ebacbf1870231ae9723fc5381581ac8d6619b
Michael <michael.wainaina@student.moringaschool.com>
2024-04-03 15:48:52 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(a, b) {
+    return a / b;
+}
+const square = (num) => {
+    return num * num;
+}
+const add = (a, b) => {
+    return a + b;
+}
+module.exports = {
+    divide,
+    square,
+    add
+};
